a6773c4cd0a0584526ab705e0916b57033f9ae55
narwhal04 <q.quek7@gmail.com>
2022-03-31 12:56:26 +0800

    working code

--- compoundIf.go
+++ compoundIf.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
-	//Insert your code here
+	fmt.Println("Input year for leap year check")
+	var yearInput int
+	var message string
+	fmt.Scan(&yearInput)
+
+	if yearInput%4 == 0 {
+		if yearInput%100 == 0 {
+			message = strconv.Itoa(yearInput) + " is a leap year"
+		} else {
+			message = strconv.Itoa(yearInput) + " is not a leap year"
+		}
+	} else {
+		message = strconv.Itoa(yearInput) + " is not a leap year"
+	}
+	fmt.Println(message)
 }
